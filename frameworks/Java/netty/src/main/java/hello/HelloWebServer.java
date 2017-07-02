b3918af96153194d1550e02ce170d5f8332bc8a4
Max Gortman <mgortman@microsoft.com>
2017-07-02 00:35:12 -0700

    234

--- frameworks/Java/netty/src/main/java/hello/HelloWebServer.java
+++ frameworks/Java/netty/src/main/java/hello/HelloWebServer.java
@@ -45,11 +45,11 @@ public class HelloWebServer {
 
 	private void doRun(EventLoopGroup loupGroup, Class<? extends ServerChannel> serverChannelClass, boolean isNative) throws Exception {
 		try {
-        SelfSignedCertificate ssc = new SelfSignedCertificate();
-		System.out.println(ssc.certificate().getAbsolutePath());
-		System.out.println(ssc.privateKey().getAbsolutePath());
-		File certFile = new File("identity.nokey.pem");
-		File keyFile = new File("identity.withkey.pem");
+        // SelfSignedCertificate ssc = new SelfSignedCertificate();
+		// System.out.println(ssc.certificate().getAbsolutePath());
+		// System.out.println(ssc.privateKey().getAbsolutePath());
+		File certFile = new File("identity.crt");
+		File keyFile = new File("identity-dec.key");
         SslContext sslCtx = SslContextBuilder.forServer(certFile, keyFile)//ssc.certificate(), ssc.privateKey())
             .build();
 
